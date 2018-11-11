cc4684f2d0c9e1727a60b20c1cbb814e1ee06606
Derrick Drake <drake.derrick13@gmail.com>
2018-11-11 16:16:06 -0500

    almost final website

---
+++ Jon Website/script.js
@@ -0,0 +1,10 @@
+function submitButton() {
+document.getElementById('submitform').submit();
+}
+function submitButton() {
+alert('Form has been submitted we will reach out within 24hrs, Thank you for your submission.');
+}
+
+function submitButton(){
+  document.getElementById('dropdown-notification').submit();
+}
